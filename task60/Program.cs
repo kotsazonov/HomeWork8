da81749f49c24f9806ed70888496645da8b88efd
kostya <kotsazonov@gmail.com>
2023-07-08 13:29:23 +0300

    добавили лве задачи

---
+++ task60/Program.cs
@@ -0,0 +1,70 @@
+﻿// Задача 60: Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить
+// массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+Console.Clear();
+
+
+int[,,] array3D = new int[6, 5, 2];
+
+CreateArray(array3D);
+PrintArray(array3D);
+
+
+void PrintArray(int[,,] array3D)
+{
+    for (int i = 0; i < array3D.GetLength(0); i++)
+    {
+        for (int j = 0; j < array3D.GetLength(1); j++)
+        {
+
+            for (int k = 0; k < array3D.GetLength(2); k++)
+            {
+                Console.Write($"{array3D[i, j, k]} ({j},{k},{i})\t");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
+
+void CreateArray(int[,,] array3D)
+{
+    int[] temp = new int[array3D.Length];
+    int number;
+    for (int i = 0; i < temp.Length; i++)
+    {
+        temp[i] = new Random().Next(10, 100);
+        number = temp[i];
+        if (i >= 1)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                while (temp[i] == temp[j])
+                {
+                    temp[i] = new Random().Next(10, 100);
+                    j = 0;
+                    number = temp[i];
+                }
+
+            }
+        }
+    }
+    int count = 0;
+    for (int x = 0; x < array3D.GetLength(0); x++)
+    {
+        for (int y = 0; y < array3D.GetLength(1); y++)
+        {
+            for (int z = 0; z < array3D.GetLength(2); z++)
+            {
+                array3D[x, y, z] = temp[count];
+                count++;
+            }
+        }
+    }
+}
---
+++ task62/Program.cs
@@ -0,0 +1,46 @@
+﻿Console.Clear();
+int size = 5;
+int[,] array = new int[size, size];
+SpiralFillArray(array, size);       // Метод № 1 - заполнение массива по спирали. 
+PrintArray(array, size);            // Метод № 2 - вывод массива на печать. 
+
+// МЕТОДЫ
+// Метод № 1 - заполнение массива по спирали. 
+void SpiralFillArray(int[,] array, int size)
+{
+    int value = 1;                // Значение элемента [0, 0].          
+    int i = 0;
+    int j = 0;
+    while (value <= size * size)  // Диапазон значений элементов массива. 
+    {
+        array[i, j] = value;
+        if (i <= j + 1 && i + j < size - 1)  // Заполнение строк слева направо.
+            j++;                             // Возврат увлеличенного значения j.
+        else if (i < j && i + j >= size - 1) // Заполнение столбцов сверху вниз. 
+            i++;                             // Возврат увлеличенного значения i.
+        else if (i >= j && i + j > size - 1) // Заполнение строк справа налево. 
+            j--;                             // Возврат уменьшенного значения j.
+        else                                 // Заполнение столбцов снизу вверх. 
+            i--;                             // Возврат уменьшенного значения i. 
+        value++;
+    }
+}
+
+//  Метод № 2 - вывод массива на печать.
+void PrintArray(int[,] array, int size)
+{
+    Console.WriteLine($"Массив, имеющий размер {size} х {size}, заполнен по спирали.");
+    Console.WriteLine();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] < 10)
+            {
+                Console.Write("0" + array[i, j] + " ");
+            }
+            else Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
